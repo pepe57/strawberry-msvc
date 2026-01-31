b86b61fad7245d5ca36d57be3ced8b8fbf7b75ed
Jonas Kvinge <jonas@jkvinge.net>
2026-01-31 18:52:33 +0100

    StrawberryMSVCBuild: Add -ErrorAction SilentlyContinue

--- StrawberryMSVCBuild.ps1
+++ StrawberryMSVCBuild.ps1
@@ -1195,8 +1195,8 @@ function Build-GnuTLS {
     MSBuildProject -project_path "..\..\nettle\SMP\libnettle.vcxproj" -configuration "Release"
     MSBuildProject -project_path "..\..\nettle\SMP\libhogweed.vcxproj" -configuration "Release"
     MSBuildProject -project_path "libgnutls.vcxproj" -configuration "ReleaseDLLStaticDeps"
-    Remove-Item "$prefix_path\lib\gnutls$lib_postfix.lib" -Force
-    Remove-Item "$prefix_path\bin\gnutls$lib_postfix.dll" -Force
+    Remove-Item "$prefix_path\lib\gnutls$lib_postfix.lib" -Force -ErrorAction SilentlyContinue
+    Remove-Item "$prefix_path\bin\gnutls$lib_postfix.dll" -Force -ErrorAction SilentlyContinue
     Copy-Item "..\..\..\msvc\lib\${arch_short}\gnutls.lib" "$prefix_path\lib\" -Force
     Copy-Item "..\..\..\msvc\bin\${arch_short}\gnutls.dll" "$prefix_path\bin\" -Force
     CreatePkgConfigFile -prefix $prefix_path_forward -name "gnutls" -description "gnutls" -url "https://www.gnutls.org/" -version $gnutls_version -libs "-L`${libdir} -lgnutls" -cflags "-I`${includedir}" -output_file "$prefix_path\lib\pkgconfig\gnutls.pc"
@@ -1216,7 +1216,7 @@ function Build-LibPNG {
     Set-Location "libpng-$libpng_version"
     & patch -p1 -N -i "$downloads_path\libpng-pkgconf.patch" 2>&1 | Out-Null
     CMakeBuild
-    Remove-Item "$prefix_path\bin\libpng16_static${lib_postfix}.lib.dll" -Force
+    Remove-Item "$prefix_path\lib\libpng16_static${lib_postfix}.lib" -Force -ErrorAction SilentlyContinue
     if ($build_type -eq "debug") {
       Copy-Item "$prefix_path\lib\libpng16d.lib" "$prefix_path\lib\png16.lib" -Force
     }
@@ -1237,8 +1237,8 @@ function Build-LibJPEG {
         "-DENABLE_SHARED=ON",
         "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
       )
-    Remove-Item "$prefix_path\lib\jpeg-static${lib_postfix}.lib" -Force
-    Remove-Item "$prefix_path\lib\turbojpeg-static${lib_postfix}.lib" -Force
+    Remove-Item "$prefix_path\lib\jpeg-static.lib" -Force -ErrorAction SilentlyContinue
+    Remove-Item "$prefix_path\lib\turbojpeg-static.lib" -Force -ErrorAction SilentlyContinue
   }
   finally {
     Pop-Location
@@ -1963,7 +1963,7 @@ function Build-LibGME {
     Set-Location libgme-$libgme_version
     & patch -p1 -N -i $downloads_path/libgme-pkgconf.patch
     CMakeBuild
-    Remove-Item "$prefix_path\lib\gme-static.lib" -Force
+    Remove-Item "$prefix_path\lib\gme-static.lib" -Force -ErrorAction SilentlyContinue
   }
   finally {
     Pop-Location
